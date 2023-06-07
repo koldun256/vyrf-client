94c8fc080693ef593ee7531c08d949089d0fb11e
koldun <egor.sharypov2005@gmail.com>
2023-06-07 13:23:22 +0000

    simple udp client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:8888")?;
+    let server = "127.0.0.1:8080";
+
+    socket.send_to(&[0x04, 0x05, 0x06], server)?;
+
+    let mut buffer = [0; 1024];
+    let (len, _) = socket.recv_from(&mut buffer)?;
+    let result = &buffer[..len];
+    for byte in result {
+        print!("{} ", byte);
+    }
+    println!();
+    Ok(())
 }
