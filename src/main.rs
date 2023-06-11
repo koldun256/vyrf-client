01526961c832984f5aa01ec0c05625caef776434
koldun <egor.sharypov2005@gmail.com>
2023-06-12 00:43:24 +0700

    changed udp client api

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::thread;
-use std::sync::mpsc;
+use std::env;
+use std::sync::mpsc::{Receiver, Sender};
 use sfml::window::{Event, Style};
 use sfml::graphics::{Text, Font, RenderWindow, RenderTarget, Color};
 use udpclient::ServerMsg;
@@ -7,47 +7,40 @@ use udpclient::ServerMsg;
 pub mod udpclient;
 
 fn main() -> std::io::Result<()> {
-    let socket = udpclient::open_udp_socket();
-    let (tx, rx) = mpsc::channel::<ServerMsg>();
-    println!("1");
+    let args: Vec<String> = env::args().collect();
+    let (udp_tx, udp_rx) = udpclient::connect(&args[1]);
+
+    let mut val = 0;
+
     let mut window = RenderWindow::new((800, 600),
                              "SFML window",
                              Style::CLOSE,
                              &Default::default());
-    let mut val = 0;
-    let font = match Font::from_file("src/resources/Lato-Regular.ttf") {
-        Some(font) => font,
-        None => {
-            panic!("Failed to read font file!");
-        }
-    };
+    let font = Font::from_file("src/resources/Lato-Regular.ttf").expect("failed to read font");
     let mut text = Text::new(&val.to_string(), &font, 16);
-    thread::scope(|s| { 
-        s.spawn(|| udpclient::start_udp_thread(tx, &socket)); 
-        while window.is_open() {
-            while let Some(msg) = rx.try_iter().next() {
-                match msg {
-                    ServerMsg::Val(new_val) => val = new_val
-                }
+
+    while window.is_open() {
+        while let Some(msg) = udp_rx.try_iter().next() {
+            match msg {
+                ServerMsg::Val(new_val) => val = new_val
             }
-            while let Some(event) = window.poll_event() {
-                match event {
-                    Event::Closed => window.close(),
-                    Event::MouseButtonPressed { button: _, x: _, y: _ } => {
-                        udpclient::send_udp_msg(udpclient::ClientMsg::Increment, &socket);
-                    },
-                    _ => ()
-                }
-                if event == Event::Closed { window.close(); }
-            }
-            window.set_active(true);
-            window.clear(Color::rgb(50, 200, 50));
-        
-            text.set_string(&val.to_string());
-            window.draw(&text);
-        
-            window.display();
         }
-    });
+        while let Some(event) = window.poll_event() {
+            match event {
+                Event::Closed => window.close(),
+                Event::MouseButtonPressed { button: _, x: _, y: _ } => {
+                    udp_tx.send(udpclient::ClientMsg::Increment);
+                },
+                _ => ()
+            }
+        }
+        window.set_active(true);
+        window.clear(Color::rgb(50, 200, 50));
+    
+        text.set_string(&val.to_string());
+        window.draw(&text);
+    
+        window.display();
+    }
     Ok(())
 }
--- src/udpclient.rs
+++ src/udpclient.rs
@@ -1,6 +1,6 @@
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{Receiver, Sender, channel};
 use std::net::UdpSocket;
-use std::io::stdin;
+use std::thread;
 
 pub enum ServerMsg {
     Val(i32)
@@ -10,24 +10,29 @@ pub enum ClientMsg {
     Increment
 }
 
-pub fn open_udp_socket() -> UdpSocket {
-    let mut port = String::new();
-    stdin().read_line(&mut port).expect("error reading port from cli");
-    port.pop();
-    UdpSocket::bind(format!("127.0.0.1:{port}")).expect("cannot open udp socket")
+fn recv_msg(buf: &[u8; 4]) -> ServerMsg {
+    ServerMsg::Val(i32::from_be_bytes(*buf))
 }
 
-pub fn start_udp_thread(tx: Sender<ServerMsg>, socket: &UdpSocket) {
-    loop {
-        let mut answer = [0; 4];
-        socket.recv_from(&mut answer).expect("error reading msg from server");
-        tx.send(ServerMsg::Val(i32::from_be_bytes(answer)));
-    }
-}
-
-pub fn send_udp_msg(msg: ClientMsg, socket: &UdpSocket) {
+fn send_msg(msg: ClientMsg, socket: &UdpSocket) {
     let server = "127.0.0.1:8080";
     match msg {
         ClientMsg::Increment => socket.send_to(&[1], server)
     };
+}
+
+pub fn connect(port: &str) -> (Sender<ClientMsg>, Receiver<ServerMsg>) {
+    let (tx1, rx1) = channel(); // from client to server
+    let (tx2, rx2) = channel(); // from server to client
+    let socket1 = UdpSocket::bind(format!("127.0.0.1:{port}")).expect("cannot open udp socket");
+    let socket2 = socket1.try_clone().unwrap();
+    thread::spawn(move || loop {
+        send_msg(rx1.recv().unwrap(), &socket1);
+    });
+    thread::spawn(move || loop {
+        let mut answer = [0; 4];
+        socket2.recv_from(&mut answer).expect("error reading msg from server");
+        tx2.send(recv_msg(&answer));
+    });
+    (tx1, rx2)
 }
